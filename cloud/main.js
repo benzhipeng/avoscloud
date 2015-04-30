b0b74cc642f7181260284c82b21e60f189a58519
benzhipeng1024 <977982077@qq.com>
2015-04-30 17:22:54 +0800

    no message

--- cloud/main.js
+++ cloud/main.js
@@ -7,7 +7,7 @@ AV.Cloud.define("hello", function(request, response) {
   // response.success(" world 111!");
   	api.fetchData(request.params.offset,request.params.tag_id,function (data) {
 
-  		console.info('data: '+ data);
+		response.success(data);
   	});
 });
 
